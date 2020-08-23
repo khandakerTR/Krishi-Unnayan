0e2100ce2092d53d38580fe586a42f3595402652
tkhandaker101 <56843185+tkhandaker101@users.noreply.github.com>
2020-08-24 00:48:24 +0600

    action_tel updated
    action_tel updated without calculation

--- masala.php
+++ masala.php
@@ -1,4 +1,13 @@
-<?php ?>
+<?php 
+include "config.php";
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: masala.php");
+    exit;
+}
+?>
 <html lang="en">
   <head>
   <style type="text/css" media="all">
